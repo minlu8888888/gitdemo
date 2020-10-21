4aa6a0294904d81ce84d8195fc5b3d2281ae50ed
lumin <928696088@qq.com>
2020-10-21 17:25:57 +0800

    new files

--- files/201812071556/docs/demo.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-var $color = $(".theme-picker").find("span");
-var target = $(".codehim-dropdown");
-$($color).click(function(){
-    thisColor = $(this).attr('class');
-    $(target).removeClass().addClass("codehim-dropdown shadow sticky" +" " + thisColor);
-    $(".dropdown-items").removeClass().addClass("dropdown-items " + thisColor);
- });
-});
